85f28a0707c1b21ba87da3b73ec24d38f09e24a6
LouiseCox <49623459+LouiseCox@users.noreply.github.com>
2019-05-24 20:22:12 -0600

    commit1

---
+++ form-mini-proj/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  surveyFormGroup = this.fb.group({
+    gender: [''],
+    name: ['', [Validators.required, Validators.pattern('[a-z,A-Z]*')]],
+    age: ['', Validators.required],
+    studyduration: [''],
+    averagegrade: ['']
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  //returns a FormControl
+  //this is called a getter function and allows you to call a function without the (), almost like calling a var
+  get nameformcontrol() {
+    return this.surveyFormGroup.get('name');
+  }
+
+}
